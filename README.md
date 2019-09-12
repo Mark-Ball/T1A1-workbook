e7ba77965f046b15a382c26e67103a21224b02ee
Mark-Ball <markball111@gmail.com>
2019-09-12 13:56:07 +1000

    added README.md and fixed bug in q_16.rb which was causing always causing query_allergies to state that you did not have an allergy

---
+++ README.md
@@ -0,0 +1,18 @@
+# T1A1 Workbook
+
+This repo contains the ruby source code for the questions that require a script to answer.
+
+The following files are included:
+
+## q_12.rb
+
+
+## q_13.rb
+
+
+## q_16.rb
+
+
+This script requires the Ruby gem tty-prompt to run. The gem can be installed by typing the following into your terminal:
+
+> gem install tty-prompt
--- q_16.rb
+++ q_16.rb
@@ -1,16 +1,5 @@
-# An allergy test produces a single numeric score which contains the information about all the allergies the person has (that they were tested for). The list of items (and their value) that were tested are:
-
-# eggs (1)
-# peanuts (2)
-# shellfish (4)
-# strawberries (8)
-# tomatoes (16)
-# chocolate (32)
-# pollen (64)
-# cats (128)
-# So if Tom is allergic to peanuts and chocolate, he gets a score of 34.
-
-# Write a program that, given a person’s score can tell them: a) whether or not they’re allergic to a given item b) the full list of allergies.
+require "tty-prompt"
+prompt = TTY::Prompt.new
 
 #identifies the highest power of two that is lower than score, returns that number
 def identify_highest_power(value)
@@ -27,19 +16,15 @@ end
 
 def detect_allergies(score)
     allergies_for_each_value = {
-        "1" => "eggs", #2**0
-        "2" => "peanuts", #2**1
-        "4" => "shellfish", #2**2
-        "8" => "strawberries", #2**3
-        "16" => "tomatoes", #2**4
-        "32" => "chocolate", #2**5
-        "64" => "pollen", #2**6
-        "128" => "cats"  #2**7
+        "1" => "eggs",
+        "2" => "peanuts",
+        "4" => "shellfish",
+        "8" => "strawberries",
+        "16" => "tomatoes",
+        "32" => "chocolate",
+        "64" => "pollen",
+        "128" => "cats"
     }
-
-    if score > 255 || score < 1 || score.class == Float
-        return "Impossible to compute"
-    end
 
     allergies = []
     while score > 0 # the loop pushes the allergy corresponding to the highest index onto the array, then subtracts that value from the score and tests again
@@ -58,35 +43,52 @@ def query_allergies(allergy, score)
         if item == allergy
             return "You are allergic to #{allergy}."
         end
-        return "You are not allergic to #{allergy}."
-        
     end
+    return "You are not allergic to #{allergy}."
 end
-
-p(detect_allergies(34))
-p(query_allergies("chocolate", 34))
-
-
-puts("What is your allergy score?")
-score = gets.chomp
-# system("clear")
-
 
 while true
-    puts("What would you like to know? (a for querying a given item, b for the full list, exit to exit)")
-    query = gets.chomp
-    if query == "a"
-        puts("What item would you like to query?")
-        response = gets.chomp
-        p(response)
-        p(query_allergies(response, score.to_i))
-    elsif query == "b"
-        puts("Your allergies are: #{detect_allergies(score.to_i)}.")
-    elsif query == "exit"
-        exit
-    else
-        puts("That was not a valid input, please enter a or b")
+    puts("What is your allergy score?")
+    score = gets.chomp
+    if score.to_i > 255 || score.to_i < 1
+        p("Impossible to compute")
+    else 
+        break
     end
 end
 
-#to do: implement ttyp-prompt
+while true
+    query = prompt.select(
+        "What would you like to know?", 
+        [
+            "Query a specific allergy", 
+            "Full list of allergies", 
+            "Exit"
+        ],
+        cycle:true,
+        symbols: {marker: ">"})
+
+    if query == "Query a specific allergy"
+        specific_allergy = prompt.select(
+            "Which allergen would you like to query?",
+            [
+                "Eggs", 
+                "Peanuts", 
+                "Shellfish", 
+                "Strawberries", 
+                "Tomatoes", 
+                "Chocolate", 
+                "Pollen", 
+                "Cats"
+            ],
+            cycle:true,
+            symbols: {marker: ">"})
+        puts(query_allergies(specific_allergy.downcase, score.to_i))
+    elsif query == "Full list of allergies"
+        puts("Your allergies are: #{detect_allergies(score.to_i)}.")
+    elsif query == "Exit"
+        exit
+    else
+        puts("Invalid input")
+    end
+end
